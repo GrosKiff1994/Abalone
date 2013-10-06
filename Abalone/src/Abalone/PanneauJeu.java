64af29a6b1b65ab5e9afe81257fa0a13ac577527
Varadiell <willy.simon@hotmail.fr>
2013-10-06 17:08:03 +0200

    Début ajout panneau

--- Abalone/src/Abalone/PanneauJeu.java
+++ Abalone/src/Abalone/PanneauJeu.java
@@ -1,7 +1,20 @@
 package Abalone;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class PanneauJeu extends JPanel {
+
+	public PanneauJeu() {
+
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.white);
+		g.fillOval(50, 50, 20, 20);
+	}
 
 }
