f504dbc5d7b447ecfdb50c6a432a41c9e59a5876
Varadiell <willy.simon@hotmail.fr>
2013-10-13 15:43:36 +0200

    Modification du skin des boules, titre fenêtre

--- Abalone/src/Application.java
+++ Abalone/src/Application.java
@@ -6,6 +6,7 @@ public class Application {
 	public static void main(String[] args) {
 		FenetreAbalone fenetre = new FenetreAbalone();
 		Partie laPartie = new Partie(fenetre);
+		fenetre.setTitle("Abalone");
 		fenetre.setVisible(true);
 		laPartie.lancerPartie();
 	}
