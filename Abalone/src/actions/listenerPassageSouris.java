bd3956f754471bc7efd47030536e43e0cb9844b5
Yann Rolland <GrosKiff@gmail.com>
2013-10-19 12:55:20 +0200

    déplacement listeners, affichage bords enlevé

---
+++ Abalone/src/actions/listenerPassageSouris.java
@@ -0,0 +1,44 @@
+package actions;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import objects.Case;
+import core.Partie.Etat;
+import display.BoutonRond;
+import display.PanneauJeu;
+
+public class listenerPassageSouris extends MouseAdapter {
+
+	@Override
+	public void mouseEntered(java.awt.event.MouseEvent e) {
+		BoutonRond leBouton = ((BoutonRond) e.getSource());
+		PanneauJeu lePanneau = leBouton.getPanneauJeu();
+		
+		int i = leBouton.getCoordI();
+		int j = leBouton.getCoordJ();
+		Case caseCourante = lePanneau.getPlateau().getCase(i, j);
+		if (caseCourante.estOccupee()) {
+			if (leBouton.getCouleurActuelle() == null) {
+				leBouton.setCouleurActuelle(BoutonRond.couleurMouseOver);
+			}
+		}
+		if (leBouton.getCouleurActuelle() == BoutonRond.couleurSelecTour) {
+			leBouton.setCouleurActuelle(BoutonRond.couleurMouseOver);
+		}
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		BoutonRond leBouton = ((BoutonRond) e.getSource());
+		
+		if (leBouton.getCouleurActuelle() == BoutonRond.couleurMouseOver) {
+			if (leBouton.getEtat() == Etat.SELECTION) {
+				leBouton.setCouleurActuelle(null);
+			} else if (leBouton.getEtat() == Etat.DEPLACEMENT) {
+				leBouton.setCouleurActuelle(BoutonRond.couleurSelecTour);
+			}
+
+		}
+	}
+}
